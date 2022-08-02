661fe4fa3d51515842e9d31be6fe6de8c83aab7e
alamin-karno <amin15-1951@diu.edu.bd>
2022-08-02 13:05:44 +0600

    Web Landing Page: App Drawer Design Change

--- lib/widgets/menu_drawer.dart
+++ lib/widgets/menu_drawer.dart
@@ -8,102 +8,126 @@ class MenuDrawer extends StatelessWidget {
     return Drawer(
       child: Container(
         color: Colors.white, //const Color(0xFF077BD7),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20),
-              InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: const Text(
-                  'Home',
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              width: double.infinity,
+              color: Colors.blue,
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Author',
                   style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xFF077BD7),
+                    fontSize: 26,
+                    color: Colors.white,
                     fontFamily: 'Raleway',
                     fontWeight: FontWeight.w900,
                     letterSpacing: 3,
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5, bottom: 5),
-                child: Divider(
-                  color: Colors.blueGrey.shade200,
-                  thickness: 2,
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: const Text(
-                  'About',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xFF077BD7),
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w900,
-                    letterSpacing: 3,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5, bottom: 5),
-                child: Divider(
-                  color: Colors.blueGrey.shade200,
-                  thickness: 2,
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: const Text(
-                  'History',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xFF077BD7),
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w900,
-                    letterSpacing: 3,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5, bottom: 5),
-                child: Divider(
-                  color: Colors.blueGrey.shade200,
-                  thickness: 2,
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: const Text(
-                  'Contact',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xFF077BD7),
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w900,
-                    letterSpacing: 3,
-                  ),
-                ),
-              ),
-              const Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text(
-                    'Copyright © 2022 | Alamin Karno',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 10),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        'Home',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF077BD7),
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 3,
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Divider(
+                        color: Colors.blueGrey.shade200,
+                        thickness: 2,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'About',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF077BD7),
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 3,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Divider(
+                        color: Colors.blueGrey.shade200,
+                        thickness: 2,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'History',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF077BD7),
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 3,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Divider(
+                        color: Colors.blueGrey.shade200,
+                        thickness: 2,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'Contact',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF077BD7),
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 3,
+                        ),
+                      ),
+                    ),
+                    const Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          'Copyright © 2022 | Alamin Karno',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
