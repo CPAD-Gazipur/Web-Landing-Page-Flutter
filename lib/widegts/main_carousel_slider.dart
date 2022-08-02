85e097e6e7f3592aa625c8079acbdde364352d10
alamin-karno <amin15-1951@diu.edu.bd>
2022-08-02 10:45:15 +0600

    Web Landing Page: Responsive Carousel Slider Added

--- lib/widegts/main_carousel_slider.dart
+++ lib/widegts/main_carousel_slider.dart
@@ -89,89 +89,92 @@ class _MainCarouselSliderState extends State<MainCarouselSlider> {
             ),
           ),
         ),
-        AspectRatio(
-          aspectRatio: 17 / 8,
-          child: Center(
-            heightFactor: 1,
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  left: screenSize.width / 8,
-                  right: screenSize.width / 8,
-                ),
-                child: Card(
-                  elevation: 5,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      top: screenSize.height / 50,
-                      bottom: screenSize.height / 50,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        for (int i = 0; i < places.length; i++)
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
+        screenSize.width < 800
+            ? Container()
+            : AspectRatio(
+                aspectRatio: 17 / 8,
+                child: Center(
+                  heightFactor: 1,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        left: screenSize.width / 8,
+                        right: screenSize.width / 8,
+                      ),
+                      child: Card(
+                        elevation: 5,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            top: screenSize.height / 50,
+                            bottom: screenSize.height / 50,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              InkWell(
-                                splashColor: Colors.transparent,
-                                hoverColor: Colors.transparent,
-                                onHover: (value) {
-                                  setState(() {
-                                    value
-                                        ? _isHovering[i] = true
-                                        : _isHovering[i] = false;
-                                  });
-                                },
-                                onTap: () {
-                                  _controller.animateToPage(i);
-                                },
-                                child: Padding(
-                                  padding: EdgeInsets.only(
-                                    top: screenSize.height / 80,
-                                    bottom: screenSize.height / 90,
-                                  ),
-                                  child: Text(
-                                    places[i],
-                                    style: TextStyle(
-                                      color: _isHovering[i]
-                                          ? Colors.blueGrey[900]
-                                          : Colors.blueGrey,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Visibility(
-                                maintainSize: true,
-                                maintainAnimation: true,
-                                maintainState: true,
-                                visible: _isSelected[i],
-                                child: AnimatedOpacity(
-                                  duration: const Duration(milliseconds: 400),
-                                  opacity: _isSelected[i] ? 1 : 0,
-                                  child: Container(
-                                    height: 5,
-                                    width: screenSize.width / 10,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.blueGrey,
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(10),
+                              for (int i = 0; i < places.length; i++)
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    InkWell(
+                                      splashColor: Colors.transparent,
+                                      hoverColor: Colors.transparent,
+                                      onHover: (value) {
+                                        setState(() {
+                                          value
+                                              ? _isHovering[i] = true
+                                              : _isHovering[i] = false;
+                                        });
+                                      },
+                                      onTap: () {
+                                        _controller.animateToPage(i);
+                                      },
+                                      child: Padding(
+                                        padding: EdgeInsets.only(
+                                          top: screenSize.height / 80,
+                                          bottom: screenSize.height / 90,
+                                        ),
+                                        child: Text(
+                                          places[i],
+                                          style: TextStyle(
+                                            color: _isHovering[i]
+                                                ? Colors.blueGrey[900]
+                                                : Colors.blueGrey,
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                  ),
+                                    Visibility(
+                                      maintainSize: true,
+                                      maintainAnimation: true,
+                                      maintainState: true,
+                                      visible: _isSelected[i],
+                                      child: AnimatedOpacity(
+                                        duration:
+                                            const Duration(milliseconds: 400),
+                                        opacity: _isSelected[i] ? 1 : 0,
+                                        child: Container(
+                                          height: 5,
+                                          width: screenSize.width / 10,
+                                          decoration: const BoxDecoration(
+                                            color: Colors.blueGrey,
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(10),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
                             ],
                           ),
-                      ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ),
-        ),
       ],
     );
   }
