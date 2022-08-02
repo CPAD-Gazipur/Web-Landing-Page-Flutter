7d2cbec216d2958b0f43206c39a602ee505fd29d
alamin-karno <amin15-1951@diu.edu.bd>
2022-08-02 10:42:15 +0600

    Web Landing Page: Responsive Feature Tiles Added

--- lib/widegts/feature_tiles.dart
+++ lib/widegts/feature_tiles.dart
@@ -14,57 +14,104 @@ class FeatureTiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        top: screenSize.height * 0.06,
-        left: screenSize.width / 15,
-        right: screenSize.width / 15,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          ...Iterable<int>.generate(assets.length).map(
-            (index) => Column(
-              children: [
-                SizedBox(
-                  height: screenSize.width / 6,
-                  width: screenSize.width / 3.8,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                        image: AssetImage(assets[index]),
-                        fit: BoxFit.cover,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: const Offset(0, 10),
-                          blurRadius: 20,
-                          spreadRadius: 5,
-                          color: Colors.grey.withOpacity(0.3),
+    return screenSize.width < 800
+        ? Padding(
+            padding: EdgeInsets.only(top: screenSize.height / 50),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  SizedBox(width: screenSize.width / 15),
+                  ...Iterable<int>.generate(assets.length).map(
+                    (index) => Row(
+                      children: [
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: screenSize.width / 2.5,
+                              width: screenSize.width / 1.5,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(5.0),
+                                child: Image.asset(
+                                  assets[index],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: screenSize.height / 70,
+                              ),
+                              child: Text(
+                                title[index],
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
+                        SizedBox(width: screenSize.width / 15),
                       ],
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: screenSize.height / 70,
-                  ),
-                  child: Text(
-                    title[index],
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w500,
-                    ),
+                ],
+              ),
+            ),
+          )
+        : Padding(
+            padding: EdgeInsets.only(
+              top: screenSize.height * 0.06,
+              left: screenSize.width / 15,
+              right: screenSize.width / 15,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ...Iterable<int>.generate(assets.length).map(
+                  (index) => Column(
+                    children: [
+                      SizedBox(
+                        height: screenSize.width / 6,
+                        width: screenSize.width / 3.8,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                              image: AssetImage(assets[index]),
+                              fit: BoxFit.cover,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: const Offset(0, 10),
+                                blurRadius: 20,
+                                spreadRadius: 5,
+                                color: Colors.grey.withOpacity(0.3),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: screenSize.height / 70,
+                        ),
+                        child: Text(
+                          title[index],
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
